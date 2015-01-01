a436f4bd2a4ca951bd76abd3ec197ac852d6e3a7
Luciano Resende <lresende@apache.org>
2015-01-01 12:51:20 -0800

    Basic ui template using Boostrap and EJS partial templates

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'Users' });
 });
 
 module.exports = router;
