d9764f161aa608a0c9e6137534aae68cf928c5b0
Luciano Resende <lresende@apache.org>
2015-01-01 11:14:25 -0800

    Add ejs-locals engine

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var engine = require('ejs-locals');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,6 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
